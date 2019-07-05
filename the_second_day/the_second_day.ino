1c7d8af71dd6fe850c66fa5876f4c37eac1a9cc2
blankhaha <52493455+blankhaha@users.noreply.github.com>
2019-07-05 18:44:29 +0800

    Add files via upload

---
+++ the_second_day/the_second_day.ino
@@ -0,0 +1,76 @@
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  Serial.begin(9600);
+}
+int income=0;
+void loop()
+{
+	if(Serial.available()>0)
+    {
+    	income=Serial.read();
+      	switch(income)
+        {
+        	case 'f':
+          		forward();
+          		break;
+          	case 'b':
+          		backward();
+          		break;
+          	case 'l':
+          		left();
+          		break;
+          	case 'r':
+          		right();
+          		break;
+          	case 's':
+          		stop();
+          		break;
+          	default:
+          		break;
+        }
+    }
+}
+
+void forward()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+}
+
+void backward()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void left()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void right()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+}
+
+void stop()
+{
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+}
